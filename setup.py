0d05c08c0be5cb5a19ca2d37a741f12890638bbd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-11-27 13:34:18 -0500

    release: Iterate the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="ocdsindex",
-    version="0.0.1",
+    version="0.0.2",
     author="Open Contracting Partnership",
     author_email="data@open-contracting.org",
     url="https://github.com/open-contracting/ocds-index",
