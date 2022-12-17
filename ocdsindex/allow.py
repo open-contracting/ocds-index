28c91c3c63ae40c9b96cbbe455c3d7b0a273497e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-12-16 19:13:45 -0500

    feat: Reduce hardcoded default value (OCDS documentation no longer has a privacy-notice page)

--- ocdsindex/allow.py
+++ ocdsindex/allow.py
@@ -7,11 +7,11 @@ import os
 
 def allow_sphinx(root, file):
     """
-    Allows all files, except the 404 page and privacy notes.
+    Allows all files, except the 404 page.
 
     :param str root: a directory path
     :param str file: a file basename
     :returns: whether to crawl the file
     :rtype: bool
     """
-    return os.path.split(root)[1] not in ("404", "privacy-notice")
+    return os.path.split(root)[1] != "404"
